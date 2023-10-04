3d4597c244e5416570055ca3fe9a61f4edc9c6f2
batsieba <batisebatekle@gmail.com>
2023-10-04 11:44:42 +0400

    update

--- Quiz_App/script.js
+++ Quiz_App/script.js
@@ -11,8 +11,8 @@ const questions = [
     {
         question: "Which is the smallest country in the world?",
         answers:[
-            {text: "Vatican City", correct: false},
-            {text: "Bhutan", correct: true},
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
             {text: "Nepal", correct: false},
             {text: "Shri Lanka", correct: false}
         ]
@@ -53,5 +53,85 @@ function startQuiz(){
 
 function showQuestion()
 {
-    let currentQuestion = questions[current]
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        ansBtn.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
 }
+
+
+function resetState()
+{
+    nextBtn.style.display = "none";
+    while(ansBtn.firstChild)
+    {
+        ansBtn.removeChild(ansBtn.firstChild);
+    }
+}
+
+function selectAnswer(e)
+{
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(ansBtn.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+
+    });
+
+    nextBtn.style.display = "block";
+}
+
+function showScore()
+{
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}.`;
+    nextBtn.innerHTML = "Play Again";
+    nextBtn.style.display = "block";
+
+}
+
+
+function handleNextButton()
+{
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length)
+    {
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+nextBtn.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length)
+    {
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
+
+startQuiz();
